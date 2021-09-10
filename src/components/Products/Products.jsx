deb7211853ad0372dae3f15dc714cd3e83e4a210
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 11:17:52 +0100

    :rocket:

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Grid from '@material-ui/core';
+
+const Products = () => {
+    <main>
+        <Grid container justify="center" spacing={4}>
+
+        </Grid>
+    </main>
+
+}
+
+export default Products
