cbbcb4615cf329969ec5e741784f3223823d2528
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 21:23:06 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Products, Navbar} from './components';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -13,9 +14,18 @@ const App = () => {
         setProducts(data);
     }
 
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve());
+
+
+    }
+
     useEffect(() =>  {
         fetchProducts();
+        fetchCart();
     }, []);
+
+    console.log(cart)
 
 
     return (
