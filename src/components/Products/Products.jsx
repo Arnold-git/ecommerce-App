968c6f22b4284e42d69e6db4a37b1c97a29be2e5
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 21:10:34 +0100

    :construction:

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,15 +5,10 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5'},
-    { id: 2, name: 'Macbook', description: 'Apple macbook', price: '$10'},
 
 
-];
 
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
     return (
         <main className={classes.content}>
