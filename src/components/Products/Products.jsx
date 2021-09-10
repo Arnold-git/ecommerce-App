16507af1e05795e9db934efa15f4d7d4fb26d32d
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 16:42:35 +0100

    :construction:

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,6 +3,7 @@ import { Grid } from '@material-ui/core';
 
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     { id: 1, name: 'Shoes', description: 'Running shoes', price: '$5'},
@@ -13,8 +14,10 @@ const products = [
 
 
 const Products = () => {
+    const classes = useStyles();
     return (
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
