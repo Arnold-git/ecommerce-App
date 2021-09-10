fbe537bf48cc73ee6d9ac96f1b52dddd9a36670b
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 12:26:40 +0100

    :rocket:

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -3,7 +3,7 @@ import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from
 import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
 
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image='' title={product.name} />
@@ -15,12 +15,14 @@ const Product = () => {
                     <Typography variant="h5">
                         {product.price}
                     </Typography>
-
-
                 </div>
+                <Typography variant="h2" color="textSecondary">{ product.description }</Typography>
             </CardContent>
-
-
+            <CardActions disableSpacing className={classes.CardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
