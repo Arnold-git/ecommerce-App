f40207147abdf6f19204597cbb0296c310f738f9
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 14:14:35 +0100

    :construction:

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
 import  { shoppingCart } from '@material-ui/core';
 
+import logo from '../../assets/cart.png'
 
 const Navbar = () => {
     return (
@@ -9,7 +10,7 @@ const Navbar = () => {
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
                     <Typography variant="h5" className={classes.title} color="inherit">
-                        <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                        <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
 
                     </Typography>
                 </Toolbar>
