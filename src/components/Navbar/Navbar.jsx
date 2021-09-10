e634279b1cf5772409028c794cb60e1bbcf14786
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 13:52:32 +0100

    :construction:

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core'
+import  { shoppingCart } from '@material-ui/core';
+
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,19 +2,18 @@ import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles(() => ({
     root: {
-        maxWidth: '100%'
-    }, 
+      maxWidth: '100%',
+    },
     media: {
-        height: 0,
-        paddingTop: '56.25%',
+      height: 0,
+      paddingTop: '56.25%',
     },
     cardActions: {
-        display: 'flex',
-        justifyContent: 'flex-end',
+      display: 'flex',
+      justifyContent: 'flex-end',
     },
     cardContent: {
-        display: 'flex',
-        justifyContent: 'space-between',
+      display: 'flex',
+      justifyContent: 'space-between',
     },
-
-}));
+  }));
