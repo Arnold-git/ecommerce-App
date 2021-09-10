7b788eb653387b6d2abda524bb65f3497c927e64
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 13:40:58 +0100

    :construction:

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -13,16 +13,17 @@ const products = [
 
 
 const Products = () => {
-    <main>
-        <Grid container justify="center" spacing={4}>
-            {products.map((product) => (
-                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
-                </Grid>
-            ))}
-
-        </Grid>
-    </main>
+    return (
+        <main>
+            <Grid container justify="center" spacing={4}>
+                {products.map((product) => (
+                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
+            </Grid>
+        </main>
+    );
 
 }
 
