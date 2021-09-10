fdba8cd2155af503b1ee60b434112c1124ec86a9
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 14:36:56 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import Products from './components/Products/Products';
+
+
+// import Products from './components/Products/Products';
+// import Navbar from './components/Navbar/Navbar';
+
+import { Products, Navbar} from './components';
 
 const Product = () => {
     return (
