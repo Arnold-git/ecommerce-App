57fe9c36c2a39be7b963bb4b97260b1bc7072c7f
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 12:00:26 +0100

    :rocket:

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core'
-import { AddShoppingCart } from '@material-ui/icons';
+import { AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
 
 
 const Product = () => {
     return (
-        <Card>
-            
+        <Card className={classes.root}>
+            <CardMedia className={classes.media} image='' title={product.name} />
+            <CardContent>
+                <div className={classes.CardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+
+
+                </div>
+            </CardContent>
+
+
         </Card>
     )
 }
