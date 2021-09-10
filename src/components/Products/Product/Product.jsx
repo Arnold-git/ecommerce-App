8bbbe864907bd7a3c0c3ae2e9e87e7d9d65827d2
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-10 11:46:20 +0100

    :rocket:

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core'
+import { AddShoppingCart } from '@material-ui/icons';
+
+
+const Product = () => {
+    return (
+        <Card>
+            
+        </Card>
+    )
+}
+
+export default Product
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -14,7 +14,7 @@ const Products = () => {
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <product/>
+                    <Product />
                 </Grid>
             ))}
 
