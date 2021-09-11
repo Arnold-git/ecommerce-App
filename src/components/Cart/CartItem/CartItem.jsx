4a9c205dcc57a7eafc9ebe49d8fb7f309593141c
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-11 20:09:40 +0100

    :construction:

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+import useStyles from './styles'
+
 
 const CartItem = () => {
     return (
