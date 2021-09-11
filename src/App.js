5c20c1af4fcba627b98ba94ff928b1b9935d84e7
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-11 22:25:29 +0100

    :construction:

--- src/App.js
+++ src/App.js
@@ -22,10 +22,20 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
+        const { cart } = await commerce.cart.add(productId, quantity);
 
-        setCart(item.cart);
+        setCart(cart);
 
+    }
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const { cart } = await commerce.cart.update(productId, { quantity });
+
+        setCart(cart)
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const { cart } = await commerce.cart.remove(productId);
     }
 
     useEffect(() =>  {
