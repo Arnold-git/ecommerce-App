b7b2b5db188888efbc0829c1c689e7df5ecf4e3d
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-11 12:55:25 +0100

    :construction:

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -23,7 +23,8 @@ export const Cart = ({ cart }) => {
             <div className={classes.cardDetails}>
                 <Typography variant="h4">Subtotal: { cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button"></Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty Cart</Button>
+                    <Button className={classes.checkout} size="large" type="button" variant="contained" color="primary">Check out</Button>
                 </div>
 
             </div>
