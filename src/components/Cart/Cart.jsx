fbfebb665ca3aa93e1db388a0ee61fafc1136103
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-11 12:21:42 +0100

    :construction:

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,10 +3,22 @@ import { Container, Typography, Button, Grid } from '@material-ui/core'
 import { CallMissedSharp } from '@material-ui/icons'
 
 export const Cart = () => {
+    const isEmpty = true;
+
+    const EmptyCart = () => (
+        <Typography variant="subtitle1">You have no item in your cart, start shopping!</Typography>
+    );
+
+    const FilledCart = () => (
+
+    )
+        
+
     return (
         <Container>
             <div className={classes.toolbar}/>
-            <Typography className={classes.title}>Your Shopping Cart</Typography>
+            <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+            { isEmpty ? <EmptyCart/> : < FilledCart/>}
         </Container>
     )
 }
