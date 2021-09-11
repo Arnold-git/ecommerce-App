76ae247ab9efa3544e2578de889219a569a12515
i_amarnold@bitbucket.org <arnoldighiwiyisi@gmail.com>
2021-09-11 20:26:50 +0100

    :construction:

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,6 +3,7 @@ import { Container, Typography, Button, Grid } from '@material-ui/core'
 
 
 import useStyles from './Styles';
+import CartItem from './CartItem/CartItem';
 
 
 const Cart = ({ cart }) => {
@@ -17,7 +18,7 @@ const Cart = ({ cart }) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
-                        <div>{ item.name}</div>
+                        <CartItem item={item}/>
 
                     </Grid>
                 ))}
--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
-import { Typography, Button, Card, CardActions, CardContent, CardMedia } from '@material-ui/core'
+import { Typography, Button, Card, CardActions, CardContent, CardMedia, MenuItem } from '@material-ui/core'
 import useStyles from './styles'
 
 
 const CartItem = () => {
+    const classes = useStyles();
+
     return (
-        <div>
-            
-        </div>
+        <Card>
+            <CardMedia image={item.media.source} alt={item.name} className={classes.media}/>
+            <CardContent className={classes.cardContent}>
+                <Typography variant="h4">{item.name}</Typography>
+                <Typography variant="h5">{item.line_total.formatted_with_symbol}</Typography>
+
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <div className={classes.button}>
+                    <Button type="button" size="small"></Button>
+                    <Typography>{item.quantity}</Typography>
+                    <Button type="button" size="small"></Button>
+
+                </div>
+
+            </CardActions>
+        </Card>
     )
 }
 
